c8a6a86ce31e0d4784c3f54ebfbee762f83a6e14
Elvis <martinelloelvis@gmail.com>
2019-05-14 12:37:56 +0200

    Initial Commit

---
+++ Modello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Modello
+//
+//  Created by larisa on 14/05/19.
+//  Copyright © 2019 Elvis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
